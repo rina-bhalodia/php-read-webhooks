921c70a68283bb33bf60147b1d77fa55b6a15996
Blag <atejada@gmail.com>
2023-12-11 08:28:47 -0500

    Update index.php

--- index.php
+++ index.php
@@ -4,8 +4,14 @@ require_once 'vendors/limonade.php';
 dispatch('/', 'hello');
   function hello()
   {
-      return 'Hello world!';
+      return 'Hello world from Limonade!';
   }
+
+dispatch('/test', 'test');
+        function test()
+        {
+            return "This should work...";
+        }
 
 dispatch('/hello_name/:name', 'hello_name');
         function hello_name()
