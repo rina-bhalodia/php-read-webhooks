3f1c8c8c0b0595c8f2565995c0ad566d0779cca0
Blag <atejada@gmail.com>
2023-12-18 04:37:02 -0500

    Update index.php

--- index.php
+++ index.php
@@ -18,20 +18,22 @@ $app->get('/webhook', function () use($app) {
 
 # Page for the Webhook to send the information to
 $app->post('/webhook', function () use($app) {
-	echo "On Post!";
 	echo "file_get_contents('php://input')";
-    $is_genuine = verify_signature(file_get_contents('php://input'),
-    utf8_encode(getenv('CLIENT_SECRET')),
-	request()->headers('X-Nylas-Signature'));
+	$GLOBALS["webhook"] = "Hey! Ho!";
+	//echo "On Post!";
+	//echo "file_get_contents('php://input')";
+    //$is_genuine = verify_signature(file_get_contents('php://input'),
+    //utf8_encode(getenv('CLIENT_SECRET')),
+	//request()->headers('X-Nylas-Signature'));
 	
-	# Is it really coming from Nylas?	
-    if(!$is_genuine){
-		response()->plain('Signature verification failed!', 401);
-    }
+	//# Is it really coming from Nylas?	
+    //if(!$is_genuine){
+		//response()->plain('Signature verification failed!', 401);
+    //}
     
-	$json = file_get_contents('php://input');
-	$data = json_decode($json, true);    
-    $GLOBALS["webhook"] = $data;
+	//$json = file_get_contents('php://input');
+	//$data = json_decode($json, true);    
+    //$GLOBALS["webhook"] = $data;
 });
 
 function verify_signature($message, $key, $signature){
