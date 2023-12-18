96ebb8541ea05b76e3ebcda81132419d2b4cc8f5
Blag <atejada@gmail.com>
2023-12-18 03:33:33 -0500

    Update index.php

--- index.php
+++ index.php
@@ -4,9 +4,31 @@ require __DIR__ . '/vendor/autoload.php';
 
 $app = new Leaf\App();
 
+# This will be called to validate our webhook
 $app->get('/webhook', function () use($app) {
 	$challenge = request()->get('challenge');
 	echo "$challenge";
 });
+
+# Page for the Webhook to send the information to
+$app->post('/webhook', function () use($app) {
+    $is_genuine = verify_signature(file_get_contents('php://input'),
+    utf8_encode(getenv('CLIENT_SECRET')),
+	request()->headers('X-Nylas-Signature'));
+	
+	# Is it really coming from Nylas?	
+    if(!$is_genuine){
+		response()->plain('Signature verification failed!', 401);
+    }
+    
+	$json = file_get_contents('php://input');
+	$data = json_decode($json, true);    
+    echo "$data";
+});
+
+function verify_signature($message, $key, $signature){
+	$digest = hash_hmac('sha256', $message, $key);
+	return(hash_equals($digest, $signature));
+}
 
 $app->run();
