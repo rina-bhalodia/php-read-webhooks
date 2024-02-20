0f07943809b81512ce6d98ad79da7960f6a432be
Blag <atejada@gmail.com>
2024-02-20 16:44:24 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,47 +1,72 @@
 <?php
 
 require __DIR__ . '/vendor/autoload.php';
+use Leaf\Blade;
 
 $app = new Leaf\App();
+$blade = new Blade('views', 'storage/cache');
+session_start();
 
-global $webhook;
-$webhook = "Nothing here";
+class Webhook
+{
+    public $id;
+    public $date;
+    public $title;
+    public $description;
+    public $participants;
+    public $status;
+}
 
-$app->get('/', function() use($app){
-	global $webhook;
-	echo "$webhook";
+$webhooks = array();
+
+$app->get('/init', function() use($app){
+  $_SESSION['webhooks'] = array();
 });
 
-# This will be called to validate our webhook
+$app->get('/', function() use($app, $blade){
+	echo $blade->render('webhooks', ['webhooks' => $_SESSION['webhooks']]);
+//foreach ($_SESSION['webhooks'] as $webhook) {
+//	foreach ($webhook as $inner_webhook) {
+//		echo "$inner_webhook->id<br>";
+//	}
+//}
+});
+
+$app->get('/clear', function() use ($app){
+	session_unset();
+});
+
+/*$app->get('/test', function() use($app, $blade){
+//echo $blade->render('webhooks', ['name' => 'Cheko Trepo']);
+global $webhooks;
+$index = count($webhooks) + 1;
+$webhooks[$index] = new Webhook();
+$webhooks[$index]->id = '123';
+$webhooks[$index]->date = '1/1/2021';
+array_push($_SESSION['webhooks'], $webhooks);
+});*/
+
+// This will be called to validate our webhook
 $app->get('/webhook', function () use($app) {
 	$challenge = request()->get('challenge');
 	echo "$challenge";
 });
 
-# Page for the Webhook to send the information to
+// Page for the Webhook to send the information to
 $app->post('/webhook', function () use($app) {
-	echo "<script>console.log('Running Post');</script>";
-	global $webhook;
-	$webhook = request()->headers('X-Nylas-Signature');
-	#$json = file_get_contents('php://input');
-	#$data = json_decode($json, true);
-	#echo "<script>console.log('".$data."');</script>";
-	#echo "file_get_contents('php://input')";
-	#$GLOBALS["webhook"] = "Hey! Ho!";
-	//echo "On Post!";
-	//echo "file_get_contents('php://input')";
-    //$is_genuine = verify_signature(file_get_contents('php://input'),
-    //utf8_encode(getenv('CLIENT_SECRET')),
-	//request()->headers('X-Nylas-Signature'));
+	$json = file_get_contents('php://input');
+	$data = json_decode($json, true);
+    $is_genuine = verify_signature(file_get_contents('php://input'),
+    utf8_encode(getenv('CLIENT_SECRET')),
+	request()->headers('X-Nylas-Signature'));
 	
-	//# Is it really coming from Nylas?	
-    //if(!$is_genuine){
-		//response()->plain('Signature verification failed!', 401);
-    //}
+	# Is it really coming from Nylas?	
+    if(!$is_genuine){
+		response()->plain('Signature verification failed!', 401);
+    }
     
-	//$json = file_get_contents('php://input');
-	//$data = json_decode($json, true);    
-    //$GLOBALS["webhook"] = $data;
+	$data = json_decode($json, true);    
+    echo $data;
 });
 
 function verify_signature($message, $key, $signature){
