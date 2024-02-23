ac468b955c637ea2a6375bc9f0ce90cc7810dc5c
Blag <atejada@gmail.com>
2024-02-23 16:54:47 -0500

    Update index.php

--- index.php
+++ index.php
@@ -49,7 +49,6 @@ $app->post('/webhook', function () use($app) {
                                  mb_convert_encoding(getenv('CLIENT_SECRET'), 'UTF-8', 'ISO-8859-1'),
                                  request()->headers('X-Nylas-Signature'));
   error_log(print_r("is_genuine: $is_genuine", true));
-  error_log(print_r($_SESSION['test'], true));  
   # Is it really coming from Nylas?	
   error_log("Before checking for is_genuine");
   if(!$is_genuine){
