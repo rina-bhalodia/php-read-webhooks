a660c0b17c7705f732cd2822406593c113cf7b1f
Blag <atejada@gmail.com>
2024-02-23 15:16:58 -0500

    Update webhooks.blade.php

--- views/webhooks.blade.php
+++ views/webhooks.blade.php
@@ -4,6 +4,7 @@
         <title>Webhooks</title>
     </head>
     <body>
+    <h1>Webhooks</h1>    
         @foreach ($webhooks as $webhook)
           @foreach ($webhook as $inner_webhook)
             <p>{{ $inner_webhook->id }} | {{ $inner_webhook->date }}</p>
