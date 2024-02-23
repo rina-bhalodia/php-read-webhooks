4478c2963f04abf6821aff5b2b1882fbb65840bd
Blag <atejada@gmail.com>
2024-02-23 06:56:57 -0500

    Update index.php

--- index.php
+++ index.php
@@ -24,56 +24,35 @@ $app->get('/init', function() use($app){
 });
 
 $app->get('/', function() use($app, $blade){
-	echo $blade->render('webhooks', ['webhooks' => $_SESSION['webhooks']]);
-//foreach ($_SESSION['webhooks'] as $webhook) {
-//	foreach ($webhook as $inner_webhook) {
-//		echo "$inner_webhook->id<br>";
-//	}
-//}
+  echo $blade->render('webhooks', ['webhooks' => $_SESSION['webhooks']]);
 });
 
 $app->get('/clear', function() use ($app){
-	session_unset();
+  session_unset();
 });
-
-/*$app->get('/test', function() use($app, $blade){
-//echo $blade->render('webhooks', ['name' => 'Cheko Trepo']);
-global $webhooks;
-$index = count($webhooks) + 1;
-$webhooks[$index] = new Webhook();
-$webhooks[$index]->id = '123';
-$webhooks[$index]->date = '1/1/2021';
-array_push($_SESSION['webhooks'], $webhooks);
-});*/
 
 // This will be called to validate our webhook
 $app->get('/webhook', function () use($app) {
-	$challenge = request()->get('challenge');
-	echo "$challenge";
+  $challenge = request()->get('challenge');
+  echo "$challenge";
 });
 
 // Page for the Webhook to send the information to
 $app->post('/webhook', function () use($app) {
-    error_log(print_r("Posting a webhook", true));	
-    $json = request()->body();
-    $json2 = file_get_contents('php://input');
-    $head = request()->headers('X-Nylas-Signature');
-    error_log(print_r($json, true));
-    error_log(print_r($json2, true));
-    error_log(print_r($head, true));
-/*	$json = file_get_contents('php://input');
-	$data = json_decode($json, true);
-    $is_genuine = verify_signature(file_get_contents('php://input'),
-    utf8_encode(getenv('CLIENT_SECRET')),
-	request()->headers('X-Nylas-Signature'));
+  $json = file_get_contents('php://input');
+  $data = json_decode($json, true);
+  $is_genuine = verify_signature(file_get_contents('php://input'),
+                                 utf8_encode(getenv('CLIENT_SECRET')),
+	                         request()->headers('X-Nylas-Signature'));
 	
-	# Is it really coming from Nylas?	
-    if(!$is_genuine){
-		response()->plain('Signature verification failed!', 401);
-    }
+  # Is it really coming from Nylas?	
+  if(!$is_genuine){
+    response()->plain('Signature verification failed!', 401);
+  }
     
-	$data = json_decode($json, true);    
-    echo $data;*/
+  $data = json_decode($json, true);    
+  error_log("$data");
+  //echo $data;
 });
 
 function verify_signature($message, $key, $signature){
