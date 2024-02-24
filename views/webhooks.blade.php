7e1b0f72f63e79419eef9d4feda5b5a41eaa0fc2
Blag <atejada@gmail.com>
2024-02-24 07:08:16 -0500

    Update webhooks.blade.php

--- views/webhooks.blade.php
+++ views/webhooks.blade.php
@@ -4,8 +4,8 @@
         <title>Webhooks</title>
     </head>
     <body>
-	<h1>Webhooks</h1>
-	    @if (!is_null($webhooks))
+    <h1>Webhooks</h1>
+        @if (!is_null($webhooks))
           @foreach ($webhooks as $webhook)
               <p>This is user {{ $webhook->id }}</p>
           @endforeach
