af9c93c410e382a4fffc96c45350d91e8a1c7c4a
Blag <atejada@gmail.com>
2024-02-24 06:57:38 -0500

    Update index.php

--- index.php
+++ index.php
@@ -17,22 +17,9 @@ class Webhook
     public $status;
 }
 
-$webhooks = array();
-
-$app->get('/init', function() use($app){
-  session()->set('item', 'Cheko');
-  //$_SESSION['webhooks'] = array();
-  //$_SESSION['test'] = "I'm a session";
-});
-
 $app->get('/', function() use($app, $blade){
-  $item = session()->get('item');
-  echo $item;
-  //echo $blade->render('webhooks', ['webhooks' => $_SESSION['webhooks']]);
-});
-
-$app->get('/clear', function() use ($app){
-  session_unset();
+  $webhooks = session()->get('webhooks');
+  echo $blade->render('webhooks', ['webhooks' => $webhooks]);
 });
 
 // This will be called to validate our webhook
@@ -47,24 +34,26 @@ $app->post('/webhook', function () use($app) {
   error_log("POST WEBHOOK");
   $json = file_get_contents('php://input', true);
   $data = json_decode($json, true);
-  error_log(print_r($json, true));
   $is_genuine = verify_signature(file_get_contents('php://input'),
                                  mb_convert_encoding(getenv('CLIENT_SECRET'), 'UTF-8', 'ISO-8859-1'),
                                  request()->headers('X-Nylas-Signature'));
-  error_log(print_r("is_genuine: $is_genuine", true));
   # Is it really coming from Nylas?	
-  error_log("Before checking for is_genuine");
   if(!$is_genuine){
     response()->status(401)->plain('Signature verification failed!');
   }
   error_log("Time to save the webhook");
+  //$webhooks[$index]->id = $data->data->object->id;
+  $webhooks = session()->get('webhooks');
+  if (is_null($webhooks)){
+	  $webhooks = array();
+  }
   $index = count($webhooks) + 1;
   $webhooks[$index] = new Webhook();
   $webhooks[$index]->id = $data->data->object->id;
-  $webhooks[$index]->date = "11-22-1977";
-  array_push($_SESSION['webhooks'], $webhooks);
-  error_log(print_r("id: $data->data->object->id", true));  
-  response()->status(200)->plain('Webhook received');;
+  $webhooks[$index]->date = '1/1/2021';
+  session()->set('webhooks', $webhooks);
+  //$webhooks[$index]->date = "11-22-1977";
+  response()->status(200)->plain('Webhook received');
 });
 
 function verify_signature($message, $key, $signature){
