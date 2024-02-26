fb9db9797c41a112a55dfde40d467d4fdd58cc48
Blag <atejada@gmail.com>
2024-02-26 06:18:58 -0500

    Update index.php

--- index.php
+++ index.php
@@ -18,6 +18,7 @@ class Webhook
 }
 
 $app->get('/', function() use($app, $blade){
+  session_start();  
   error_log( print_r(session()->get('webhooks'), true) );
   var_dump(session()->get('webhooks'));
   $webhooks = session()->get('webhooks');
@@ -32,6 +33,7 @@ $app->get('/webhook', function () use($app) {
 
 // Page for the Webhook to send the information to
 $app->post('/webhook', function () use($app) {
+  session_start();  
   error_log("POST WEBHOOK");
   $json = file_get_contents('php://input', true);
   $data = json_decode($json);
