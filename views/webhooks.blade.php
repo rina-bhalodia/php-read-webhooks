d6bc13f9422bad420ac665a9bbff59ead12ab688
Blag <atejada@gmail.com>
2024-03-02 07:19:00 -0500

    Update webhooks.blade.php

--- views/webhooks.blade.php
+++ views/webhooks.blade.php
@@ -10,24 +10,20 @@
        <tr class="bg-green-600 border-green-600 border-b p-4 m-4 rounded">
            <th>Id</th>
            <th>Date</th>
-           <th>Title</th>
-           <th>Description</th>
-           <th>Participants</th>
-           <th>Status</th>
+           <th>Subject</th>
+           <th>From Email</th>
+           <th>From Name</th>
        </tr>
         @if (!is_null($webhooks))
           @foreach ($webhooks as $webhook["webhook"])
             @foreach ($webhook["webhook"] as $webhook_elem)
-              @foreach ($webhook_elem as $webhook_line)
                 <tr class="bg-white-600 border-green-600 border-b p-4 m-4 rounded">
-                  <td><p class="text-sm font-semibold">{{$webhook_line["id"]}}</p></td>
-                  <td><p class="text-sm font-semibold">{{$webhook_line["date"]}}</p></td>
-                  <td><p class="text-sm font-semibold">{{$webhook_line["title"]}}</p></td>
-                  <td><p class="text-sm font-semibold">{{$webhook_line["description"]}}</p></td>
-                  <td><p class="text-sm font-semibold">{{$webhook_line["participants"]}}</p></td>
-                  <td><p class="text-sm font-semibold">{{$webhook_line["status"]}}</p></td>
+                  <td><p class="text-sm font-semibold">{{$webhook_elem["id"]}}</p></td>
+                  <td><p class="text-sm font-semibold">{{$webhook_elem["date"]}}</p></td>
+                  <td><p class="text-sm font-semibold">{{$webhook_elem["subject"]}}</p></td>
+                  <td><p class="text-sm font-semibold">{{$webhook_elem["from_email"]}}</p></td>
+                  <td><p class="text-sm font-semibold">{{$webhook_elem["from_name"]}}</p></td>
                 </tr>
-               @endforeach
             @endforeach
           @endforeach
         @endif 
